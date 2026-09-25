0cff550000378d61ade03a82757c930ae1e3343b
Sébastien Pernecker <pernecker@gmail.com>
2026-09-25 23:00:56 +0200

    bump to 1.1.2

--- Glaneur/__init__.py
+++ Glaneur/__init__.py
@@ -1,4 +1,4 @@
 """Téléchargeur d'images multi-sources (WordPress, Djangoplicity…)."""
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __all__ = ["config", "engine", "scheduler", "sources", "systeme"]
